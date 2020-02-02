119805342e2f7351c2967090c461e5a9781f5efb
George Chen <georgechen1338@gmail.com>
2020-02-02 19:00:49 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,18 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+board = Array.new(9, " ")
+
+def input_to_index(user_input)
+  user_input.strip.to_i - 1
+end
+
+def move(board, index, char)
+  board[index] = char
+end
+
+#def get_move
+#   puts "please enter 1-9"
+#   their_move 
+
 # code your input_to_index and move method here!
